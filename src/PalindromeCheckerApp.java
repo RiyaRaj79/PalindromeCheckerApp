3f4a7497f56e8fe79df1e3d4aefa62320a0b4b95
RiyaRaj79 <rr5531@srmist.edu.in>
2026-02-16 11:11:24 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+/*
+*@author dev
+* *@Version 1.0
+ */
+
+
+public class PalindromeCheckerApp {
+     public static void main(String[] args){
+         System.out.println("PalindromeCheckerapp");
+         System.out.println("Version: 1.0");
+         System.out.println("system initialized sucessfully");
+     }
+}
+
+
+
