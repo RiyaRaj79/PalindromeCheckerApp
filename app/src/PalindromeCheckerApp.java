121f06d4ebe0d2bfe2df712ad2cfcef420d9cc5e
RiyaRaj79 <rr5531@srmist.edu.in>
2026-03-02 09:21:41 +0530

    uc3- reverse palindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,17 +1,25 @@
-public class PalindromeCheckerApp{
-        public static void main(String[] args) {
-            String text = "madam";
-            String reversed = "";
-            for (int i = text.length() - 1; i >= 0; i--) {
-                reversed = reversed + text.charAt(i);
-            }
-            if (text.equals(reversed)) {
-                System.out.println("Input text: " + text);
-                System.out.println("Is it Palindrome? : true");
-            } else {
-                System.out.println("Input text: " + text);
-                System.out.println("Is it Palindrome? : false");
-            }
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
+        if (original.equals(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        sc.close();
+    }
 }
